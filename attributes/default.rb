543cfcfe61b85e728a6514521eb8dca4dcc8e332
Peter Mooshammer <pmoosh@yahoo.com>
2014-09-27 20:24:45 -0700

    added authentication

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,27 +5,30 @@ default['java']['install_flavor'] = 'openjdk'
 default['java']['jdk_version'] = '7'
 
 normal['elasticsearch']['version'] = '1.3.2'
-normal['elasticsearch']['filename'] = "elasticsearch-#{node.elasticsearch['version']}.tar.gz"
-normal['elasticsearch']['download_url'] = [node.elasticsearch['host'], node.elasticsearch['repository'], node.elasticsearch['filename']].join('/')
+normal['elasticsearch']['filename'] = "elasticsearch-#{node['elasticsearch']['version']}.tar.gz"
+normal['elasticsearch']['download_url'] = [node['elasticsearch']['host'], node['elasticsearch']['repository'], node['elasticsearch']['filename']].join('/')
 
 default['eas-logstash']['es_server'] = '127.0.0.1'
 default['eas-logstash']['es_port'] = '9200'
 default['eas-logstash']['webserver_hostname'] = node.name
-default['eas-logstash']['webserver_listen'] = node.ipaddress
-default['eas-logstash']['webserver_aliases'] = [node.ipaddress]
+default['eas-logstash']['webserver_listen'] = node['ipaddress']
+default['eas-logstash']['webserver_aliases'] = [node['ipaddress']]
 default['eas-logstash']['webserver_port'] = 80
 default['eas-logstash']['webserver_scheme'] = 'http://'
 default['eas-logstash']['web_dir'] = '/opt/kibana/current'
+default['eas-logstash']['es_scheme'] = 'http://'
 
 default['eas-logstash']['nginx']['ssl'] = false
-default['eas-logstash']['nginx']['passwd'] = false
-#<> The path to the SSL certificate file.
+default['eas-logstash']['nginx']['passwd'] = true
+default['eas-logstash']['nginx']['htpasswd_file'] = '/etc/nginx/conf.d/kibana_user'
+
+# The path to the SSL certificate file.
 default['eas-logstash']['nginx']['ssl_certificate']     = nil
 default['eas-logstash']['nginx']['client_max_body'] = '50M'
 
-#<> The port on which to bind nginx.
+# The port on which to bind nginx.
 default['eas-logstash']['nginx']['listen_http']  = 80
-#<> The HTTPS port on which to bind nginx.
+# The HTTPS port on which to bind nginx.
 default['eas-logstash']['nginx']['listen_https'] = 443
 
 normal['nginx']['default_site_enabled'] = false
--- recipes/_nginx.rb
+++ recipes/_nginx.rb
@@ -6,9 +6,10 @@ template "#{node['nginx']['dir']}/sites-available/kibana" do
   variables(
     'ssl' => node['eas-logstash']['nginx']['ssl'],
     'passwd' => node['eas-logstash']['nginx']['passwd'],
+    'htpasswd_file' => node['eas-logstash']['nginx']['htpasswd_file'],
     'listen_http' => node['eas-logstash']['nginx']['listen_http'],
     'listen_https' => node['eas-logstash']['nginx']['listen_https'],
-    'client_max_body' => node['eas-logstash']['nginx']['client_max_body'], 
+    'client_max_body' => node['eas-logstash']['nginx']['client_max_body'],
     'es_server' => node['eas-logstash']['es_server'],
     'es_port' => node['eas-logstash']['es_port'],
     'server_name' => node['eas-logstash']['webserver_hostname'],
@@ -18,6 +19,16 @@ template "#{node['nginx']['dir']}/sites-available/kibana" do
     'listen_port' => node['eas-logstash']['webserver_port'],
     'es_scheme' => node['eas-logstash']['es_scheme']
   )
+end
+
+kibana_login = data_bag_item('users', 'kibana_user')
+
+file node['eas-logstash']['nginx']['htpasswd_file'] do
+  content "#{kibana_login['id']}:#{kibana_login['password']}"
+  owner 'root'
+  group 'root'
+  mode 00644
+  notifies :restart, 'service[nginx]'
 end
 
 nginx_site 'kibana'
